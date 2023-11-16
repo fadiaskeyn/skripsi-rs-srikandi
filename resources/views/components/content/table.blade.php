68db62e87481c02bae5d5b9324f1ad7136122916
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-11-16 20:23:24 +0700

    edit & delete gan

--- resources/views/components/content/table.blade.php
+++ resources/views/components/content/table.blade.php
@@ -15,8 +15,8 @@
                         <td>{{ $cell }}</td>
                     @endforeach
                     <td>
-                        <a class="inline-flex  items-center px-4 py-2 rounded-lg bg-theme-border-sidebar hover:bg-gray-700 text-white text-sm md:text-left font-medium" href="/edit/{{ $row['id'] }}">Edit</a> <!-- Tautan untuk mengedit -->
-                        <a class="inline-flex items-center px-4 py-2 rounded-lg bg-theme-border-sidebar hover:bg-gray-700 text-white text-sm md:text-left font-medium" href="/delete/{{ $row['id'] }}">Hapus</a> <!-- Tautan untuk menghapus -->
+                        <a class="inline-flex  items-center px-4 py-2 rounded-lg bg-theme-border-sidebar/20 text-theme-border-sidebar text-sm md:text-left font-medium" href="/edit/{{ $row['id'] }}">Edit</a> <!-- Tautan untuk mengedit -->
+                        <a class="inline-flex items-center px-4 py-2 rounded-lg bg-theme-border-sidebar text-white text-sm md:text-left font-medium" href="/delete/{{ $row['id'] }}">Hapus</a> <!-- Tautan untuk menghapus -->
                     </td>
                 </tr>
             @endforeach
