9f4ad15e12a8ee08a17c30d9b3fc2574a8830d2b
destroylord <masapin68@gmail.com>
2023-11-22 08:42:02 +0700

    census: move file to cencus

--- resources/views/pages/census/index.blade.php
+++ resources/views/pages/census/index.blade.php
@@ -1,52 +1,84 @@
-@push('style-injection')
-    <style>
-table, th, td {
-  border:1px solid black;
-}
-</style>
-@endpush
-
 @extends('layouts.app')
 @section('content')
-    
-<table style="width:100%">
-    <tr>
-        <th rowspan="5">Tanggal</th>
-        <th rowspan="5">Pasien Masuk</th>
-        <th rowspan="5">Pasien Pindahan</th>
-        <th rowspan="5">Jumlah</th>
-    </tr>
-    <tr>
-        <th colspan="5">Pasien Keluar</th>
-    </tr>
-    <tr>
-        <th colspan="3">Pasien Keluar Hidup</th>
-        <th colspan="2">Pasien Keluar Mati</th>
-    </tr>
-    <tr>
-        <th colspan="1">Pulang Paksa</th>
-        <th colspan="1">Atas Perintah Dokter</th>
-        <th colspan="1">Dirujuk</th>
-        <th colspan="1">Pasien Mati < 48 Jam</th>
-        <th colspan="1">Pasien Mati > 48 Jam</th>
-        <th rowspan="5">Total Pasien Keluar</th>
-        <th rowspan="5">Hari Perawatan</th>
-        <th rowspan="5">Lama Rawat</th>
-    </tr>
-     {{-- <tr>
-        <th rowspan="2">Cara Bayar</th>
-        <td>Umum</td>
-        <td>BPJS</td>
-        <td>ASURANSI</td>
-    </tr>    --}}
-    {{-- <tr>
-        <th rowspan="2">Kelas</th>
-        <td>VIP</td>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-    </tr> - --}}
-    <!-- Menambahkan 12 baris kosong -->
-</table>
-
+    <div class="w-full bg-white">
+        <div class="space-y-8">
+            <div class="head">
+                <h2 class="text-3xl font-bold">Data Rekapitulasi Sensus Harian</h2>
+            </div>
+            <div class="line h-2 rounded-full w-full bg-theme-border-sidebar/20">
+                <div class="line h-2 rounded-full w-2/4 bg-theme-border-sidebar"></div>
+            </div>
+        </div>
+        <div class="mt-5 max-w-2xl space-y-5">
+            <x-forms.select name="ruangan" :options="[]" id="ruangan" label="Ruangan" placeholder="Semua Ruangan" />
+            <div class="flex gap-5 max-w-lg">
+                <x-forms.input name="tanggal" type="date"  id="tanggal" label="Tanggal" />
+                <x-forms.input name="tanggal" type="date"  id="tanggal" label="" />
+            </div>
+        </div>
+        <div class="shadow border p-5 mt-20 bg-white">
+            <div class="p-2 lg:flex grid space-y-5 grid-cols-1 justify-between">
+                <div class="mt-3 w-full">
+                    <h2 class="text-2xl font-bold">Table Rekapitulasi Sensus Harian</h2>
+                </div>
+                <div class="w-full flex justify-end gap-5">
+                    <a href="#" class="px-7 py-3 text-white rounded-lg bg-theme-border-sidebar">Download PDF<span class="ml-4 mt-4"><iconify-icon icon="octicon:plus-16" class="text-sm"></iconify-icon></span></a>
+                    <a href="#" class="px-7 py-3 text-white rounded-lg bg-theme-border-sidebar">Download Excel<span class="ml-4 mt-4"></span></a>
+                    <a href="#" class="px-7 py-3 text-white rounded-lg bg-theme-border-sidebar">Print<span class="ml-4 mt-4"></span></a>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <div class="input-box-search flex mt-5 border">
+                    <button class="px-3 py-2 text-lg"><iconify-icon icon="clarity:search-line"></iconify-icon></button>
+                    <input type="search" placeholder="Search" class="p-2 outline-none w-full">
+                </div>
+            </div>
+            {{-- Table --}}
+            <table class="tables w-full">
+                <thead>
+                    <tr>
+                        <th rowspan="3">No.</th>
+                        <th rowspan="3">Tanggal</th>
+                        <th rowspan="3">Pasien Masuk</th>
+                        <th rowspan="3">Pasien Pindah</th>
+                        <th rowspan="3">Jumlah</th>
+                        <th colspan="5">Pasien Keluar</th>
+                        <th rowspan="3">Total Pasien Keluar</th>
+                        <th rowspan="3">Hari Perawatan</th>
+                        <th rowspan="3">Lama Rawat</th>
+                        <th colspan="3">Cara bayar</th>
+                        <th colspan="4">Kelas</th>
+                    </tr>
+                    <tr>
+                        <th colspan="3">Pasien Keluar Hidup</th>
+                        <th colspan="2">Pasien Keluar Mati</th>
+                        <th rowspan="2">UMUM</th>
+                        <th rowspan="2">BPJS</th>
+                        <th rowspan="2">ASURANSI</th>
+                        <th rowspan="2">VIP</th>
+                        <th rowspan="2">I</th>
+                        <th rowspan="2">II</th>
+                        <th rowspan="2">III</th>
+                    </tr>
+                    <tr>
+                        <th class="">Pulang Paksa</th>
+                        <th class="">Atas Perintah Dokter</th>
+                        <th class="">Dirujuk</th>
+                        <th class="">Mati < 48 Jam</th>
+                        <th class="">Mati > 48 Jam</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 0; $i < 10; $i++)
+                        <tr>
+                            @for ($j = 0; $j < 20; $j++)
+                                <td></td>
+                            @endfor
+                        </tr>
+                    @endfor
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
